2d46e88a87d99f4f75b97e705ecb1165b234ec3d
Abdu-Lateef Lekan-Fadeyi <tomiwafadeyi@gmail.com>
2025-05-15 21:06:14 -0400

    Update resume and experience

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -7,6 +7,20 @@ function Experience() {
       <div className="md:w-[80%] max-w-[1400px] mx-auto">
         <div className="text-center pt-8 pb-3">
           <div className="inline-block object-contain rounded-lg w-20 h-20 overflow-hidden">
+            <img src="D2AI-logo.jpg" />
+          </div>
+        </div>
+        <div className="ml-2 text-center mb-5">
+          <div className="font-semibold text-lg">D2AI.ca</div>
+          <span>ML/AI Software Engineer Intern | May 2025 - Present</span>
+        </div>
+        <p className="px-2 md:px-8 pb-8 text-center">
+        Developed full-stack authentication features using FastAPI, Next.js, and React Hook Form with JWT and SendGrid integration. Automated Postman workflows using JavaScript for seamless token management. Built a TensorFlow-based video analysis pipeline using object detection.
+        </p>
+      </div>
+      <div className="md:w-[80%] max-w-[1400px] mx-auto">
+        <div className="text-center pt-8 pb-3">
+          <div className="inline-block object-contain rounded-lg w-20 h-20 overflow-hidden">
             <img src="https://media.glassdoor.com/sqll/2858115/outlier-ai-squareLogo-1715217719094.png" />
           </div>
         </div>
